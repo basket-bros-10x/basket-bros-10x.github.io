1557508b4f5f978b8af563c86b3e4b8859ae3e37
class-80x <cansuakcay12@gmail.com>
2025-11-04 18:14:20 +0300

    oık

---
+++ assets/data/oyunlar.js
@@ -0,0 +1,819 @@
+/* Auto-generated: list.json -> oyunlar.js */
+window.oyunlar = {
+  "1": {
+    "label": "Stickman Hook",
+    "game_image_icon": "/assets/img/stickman.jpeg",
+    "game_url": "/g4m3s/hookingman/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "2": {
+    "label": "1v1.lol",
+    "game_image_icon": "/assets/img/1v1.webp",
+    "game_url": "/g4m3s/1a3b7c9e1f2g4h5j6k8l0m1n2p3q4r5s",
+    "categories": [
+      "Action"
+    ]
+  },
+  "3": {
+    "label": "Buildnow.gg",
+    "game_image_icon": "/assets/img/builf.png",
+    "game_url": "/g4m3s/a3f5d9e7c2b8a4e1f3d5c7e9b0a4f6c8/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "4": {
+    "label": "Cookie Clicker",
+    "game_image_icon": "/assets/img/cookieclicker.webp",
+    "game_url": "/g4m3s/cookie-clicker/",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "5": {
+    "label": "Retro Football",
+    "game_image_icon": "/assets/img/retrobowl.jpeg",
+    "game_url": "/g4m3s/retrobowl/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "6": {
+    "label": "Agar.io",
+    "game_image_icon": "/assets/img/agar.png",
+    "game_url": "/g4m3s/jptr-agario/web/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "7": {
+    "label": "Smash Karts",
+    "game_image_icon": "/g4m3s/d2f6b8c9e4a1f7d5a3c9e1b8f6d4a2e3/9c9e529b14731be871b07b89660bbc2a.png",
+    "game_url": "/g4m3s/d2f6b8c9e4a1f7d5a3c9e1b8f6d4a2e3",
+    "categories": [
+      "Car"
+    ]
+  },
+  "8": {
+    "label": "Slope",
+    "game_image_icon": "/assets/img/slope2.png",
+    "game_url": "/g4m3s/slope",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "9": {
+    "label": "Yohoho.io",
+    "game_image_icon": "/assets/img/pirates2.jfif",
+    "game_url": "/g4m3s/pirates",
+    "categories": [
+      "Action"
+    ]
+  },
+  "10": {
+    "label": "Gun Spin",
+    "game_image_icon": "/assets/img/gunspin.png",
+    "game_url": "/g4m3s/gunspin",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "11": {
+    "label": "Monkey Mart",
+    "game_image_icon": "/assets/img/monkey.jfif",
+    "game_url": "/g4m3s/ihatemonkeys",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "12": {
+    "label": "Awesome Tanks 2",
+    "game_image_icon": "/assets/img/tanks2.jfif",
+    "game_url": "/g4m3s/awesometanks2/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "13": {
+    "label": "Time Shooter 3",
+    "game_image_icon": "/assets/img/superhot.jfif",
+    "game_url": "/g4m3s/time-shooter-3",
+    "categories": [
+      "Action"
+    ]
+  },
+  "14": {
+    "label": "Sling Drift",
+    "game_image_icon": "/assets/img/slingdrift.png",
+    "game_url": "/g4m3s/badtaxidriver",
+    "categories": [
+      "Car"
+    ]
+  },
+  "15": {
+    "label": "Drift Boss",
+    "game_image_icon": "/assets/img/drift.jpeg",
+    "game_url": "/g4m3s/germany-drift",
+    "categories": [
+      "Car"
+    ]
+  },
+  "16": {
+    "label": "Bank Robbery",
+    "game_image_icon": "/assets/img/bank4.png",
+    "game_url": "/g4m3s/bank",
+    "categories": [
+      "Action"
+    ]
+  },
+  "17": {
+    "label": "Ovo",
+    "game_image_icon": "/assets/img/ovo.png",
+    "game_url": "/g4m3s/ovo",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "18": {
+    "label": "Tiny Fishing",
+    "game_image_icon": "/assets/img/tinyfishing.png",
+    "game_url": "/g4m3s/tiny-fishing",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "19": {
+    "label": "Vex 6",
+    "game_image_icon": "/assets/img/vex6.png",
+    "game_url": "/g4m3s/vex6",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "20": {
+    "label": "Basketball Stars",
+    "game_image_icon": "/assets/img/basketball.jpeg",
+    "game_url": "/g4m3s/basketball-stars",
+    "categories": [
+      "Action"
+    ]
+  },
+  "21": {
+    "label": "Super Mario 64",
+    "game_image_icon": "/assets/img/sm64.jpg",
+    "game_url": "/g4m3s/sm64",
+    "categories": [
+      "Adventure"
+    ]
+  },
+  "22": {
+    "label": "60 Second Burger Run",
+    "game_image_icon": "/assets/img/burger.png",
+    "game_url": "/g4m3s/60min/index.html",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "23": {
+    "label": "Paper.io 2",
+    "game_image_icon": "/assets/img/paper.jpg",
+    "game_url": "/g4m3s/paper-io-2/index.html",
+    "categories": [
+      "Action"
+    ]
+  },
+  "24": {
+    "label": "Cat Mario",
+    "game_image_icon": "/assets/img/cat.png",
+    "game_url": "/g4m3s/cat-mario/index.html",
+    "categories": [
+      "Action"
+    ]
+  },
+  "25": {
+    "label": "Idle Breakout",
+    "game_image_icon": "/assets/img/idle.png",
+    "game_url": "/g4m3s/idle-breakout/",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "26": {
+    "label": "Ducklife",
+    "game_image_icon": "/assets/img/duck.jpeg",
+    "game_url": "/g4m3s/ducklife1/index.html",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "27": {
+    "label": "Ducklife2",
+    "game_image_icon": "/assets/img/ducklife2.webp",
+    "game_url": "/g4m3s/ducklife2",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "28": {
+    "label": "Ducklife3",
+    "game_image_icon": "/assets/img/ducklife3.webp",
+    "game_url": "/g4m3s/ducklife3",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "29": {
+    "label": "Pixel Gun 2",
+    "game_image_icon": "/assets/img/pixelgun.jfif",
+    "game_url": "/g4m3s/pixelgun/maxwellstevenson.com.html",
+    "categories": [
+      "Action"
+    ]
+  },
+  "30": {
+    "label": "Drive Mad",
+    "game_image_icon": "/assets/img/drivemad.jpeg",
+    "game_url": "/g4m3s/drive-mad/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "31": {
+    "label": "Cluster Rush",
+    "game_image_icon": "/assets/img/bustergush.png",
+    "game_url": "/g4m3s/bustergush",
+    "categories": [
+      "Action"
+    ]
+  },
+  "32": {
+    "label": "10 Minutes Till Dawn",
+    "game_image_icon": "/assets/img/20min.jfif",
+    "game_url": "/g4m3s/10-minutes-till-dawn/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "33": {
+    "label": "Google Snake",
+    "game_image_icon": "/assets/img/drakethesnake.png",
+    "game_url": "/g4m3s/google-snake/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "34": {
+    "label": "Gun Mayhem 2",
+    "game_image_icon": "/assets/img/gunmayhem.jpeg",
+    "game_url": "/g4m3s/gunmayhem2/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "35": {
+    "label": "Generic Fishing",
+    "game_image_icon": "/assets/img/fishing.png",
+    "game_url": "/g4m3s/fishing_gxme/",
+    "categories": [
+      "Adventure"
+    ]
+  },
+  "36": {
+    "label": "FNAF",
+    "game_image_icon": "/assets/img/fnaf.webp",
+    "game_url": "/g4m3s/scarybear/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "37": {
+    "label": "Ninja vs Evilcorp",
+    "game_image_icon": "/assets/img/ninja.png",
+    "game_url": "/g4m3s/ninja.html",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "38": {
+    "label": "CSGO Clicker",
+    "game_image_icon": "/assets/img/csgo.png",
+    "game_url": "/g4m3s/csgo_clicker/",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "39": {
+    "label": "Madalin Stunt Cars",
+    "game_image_icon": "/assets/img/mcm.png",
+    "game_url": "/g4m3s/mcm-files/",
+    "categories": [
+      "Car"
+    ]
+  },
+  "40": {
+    "label": "Bitlife",
+    "game_image_icon": "/assets/img/bitlife.jpeg",
+    "game_url": "/g4m3s/bitlife/",
+    "categories": [
+      "Misc"
+    ]
+  },
+  "41": {
+    "label": "Snowrider",
+    "game_image_icon": "/assets/img/snow.png",
+    "game_url": "/g4m3s/Snow-Rider3D/index.html",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "42": {
+    "label": "Jetpack Joyride",
+    "game_image_icon": "/assets/img/jet.png",
+    "game_url": "/g4m3s/jetpack-joyride/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "43": {
+    "label": "Impossible Quiz",
+    "game_image_icon": "/assets/img/averageschool.png",
+    "game_url": "/g4m3s/impossiblequiz/",
+    "categories": [
+      "Misc"
+    ]
+  },
+  "44": {
+    "label": "Pappas Pizzaria",
+    "game_image_icon": "/g4m3s/papaspizzaria/papaspizzaria.jpg",
+    "game_url": "/g4m3s/papaspizzaria/",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "45": {
+    "label": "Pappas Freezeria",
+    "game_image_icon": "/assets/img/papas-freezeria.webp",
+    "game_url": "/g4m3s/papasfreezeria/",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "46": {
+    "label": "Pappas Pancakearia",
+    "game_image_icon": "/assets/img/papaspancakes.png",
+    "game_url": "/g4m3s/papaspancakeareia/",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "47": {
+    "label": "World Hardest gm 2",
+    "game_image_icon": "/g4m3s/worlds-hardest-gxme-2/the-worlds-hardest-gxme-2.jpg",
+    "game_url": "/g4m3s/worlds-hardest-gxme-2/index.html",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "48": {
+    "label": "Rooftop Snipers",
+    "game_image_icon": "/assets/img/rooft.png",
+    "game_url": "/g4m3s/rooftop-snipers/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "49": {
+    "label": "Stack",
+    "game_image_icon": "/g4m3s/stack/stack.png",
+    "game_url": "/g4m3s/stack/",
+    "categories": [
+      "Misc"
+    ]
+  },
+  "50": {
+    "label": "Tanuki Sunset",
+    "game_image_icon": "/g4m3s/tanuki-sunset/img/logo.png",
+    "game_url": "/g4m3s/tanuki-sunset/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "51": {
+    "label": "Soccer Random",
+    "game_image_icon": "/g4m3s/soccer-random/splash.png",
+    "game_url": "/g4m3s/soccer-random/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "52": {
+    "label": "Subway Surfers",
+    "game_image_icon": "/g4m3s/subway-surfers/img/splash.jpg",
+    "game_url": "/g4m3s/subway-surfers/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "53": {
+    "label": "Learn to Fly",
+    "game_image_icon": "/assets/img/learntofly.jpeg",
+    "game_url": "/g4m3s/learntofly/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "54": {
+    "label": "Learn2fly",
+    "game_image_icon": "/assets/img/learn.jpeg",
+    "game_url": "/g4m3s/learn2fly/index.html",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "55": {
+    "label": "Hilldraw",
+    "game_image_icon": "/g4m3s/hilldraw/icons/icon-512.png",
+    "game_url": "/g4m3s/hilldraw/",
+    "categories": [
+      "Car"
+    ]
+  },
+  "56": {
+    "label": "Snowball io",
+    "game_image_icon": "/assets/img/snowgamingisdead.jpg",
+    "game_url": "/g4m3s/snowballio/",
+    "categories": [
+      "Car"
+    ]
+  },
+  "57": {
+    "label": "Fireboy and water girl",
+    "game_image_icon": "/assets/img/fireandwater.jpg",
+    "game_url": "/g4m3s/fireboywatergirlforesttemple/",
+    "categories": [
+      "Adventure"
+    ]
+  },
+  "58": {
+    "label": "Pixel Speedrun",
+    "game_image_icon": "/assets/img/pixelspeedrun.png",
+    "game_url": "/g4m3s/pixellyballs/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "59": {
+    "label": "A Dance of Fire and Ice",
+    "game_image_icon": "/g4m3s/dances/splash.png",
+    "game_url": "/g4m3s/dances/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "60": {
+    "label": "Giraffes Volleyball",
+    "game_image_icon": "/g4m3s/7a9b3c4e2f6d1e8c5a2f4b7d9e3c0a6f/favico.png",
+    "game_url": "/g4m3s/7a9b3c4e2f6d1e8c5a2f4b7d9e3c0a6f",
+    "categories": [
+      "Action"
+    ]
+  },
+  "61": {
+    "label": "Deepest Sword",
+    "game_image_icon": "/assets/img/deepest-sword.png",
+    "game_url": "/g4m3s/deepswordayo/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "62": {
+    "label": "Bottle Flip 3d",
+    "game_image_icon": "/assets/img/sddefault.jpg",
+    "game_url": "/g4m3s/bottleflippingdiedin20126/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "63": {
+    "label": "Choppy Orc",
+    "game_image_icon": "/assets/img/thumb_2.jpg",
+    "game_url": "/g4m3s/ChoppyOrc",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "64": {
+    "label": "Minecraft (1.5)",
+    "game_image_icon": "/assets/img/imgoingtoslapmartin.png",
+    "game_url": "/g4m3s/precision-client/precision-client-v34.6/public/",
+    "categories": [
+      "Adventure"
+    ]
+  },
+  "65": {
+    "label": "Funny Shooter",
+    "game_image_icon": "/assets/img/funnyshooter.webp",
+    "game_url": "/g4m3s/VAzg42bVjmum4loaFEocbmzmkIYQggJd/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "66": {
+    "label": "Hole io",
+    "game_image_icon": "/assets/img/hole.io.jfif",
+    "game_url": "/g4m3s/hole.io/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "67": {
+    "label": "Tomb of the mask",
+    "game_image_icon": "/assets/img/tomb-of-the-mask-logo.jpeg",
+    "game_url": "/g4m3s/totm",
+    "categories": [
+      "Action"
+    ]
+  },
+  "68": {
+    "label": "Doge Miner",
+    "game_image_icon": "/g4m3s/doge/img/dogeminer_300x300.png",
+    "game_url": "/g4m3s/doge/",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "69": {
+    "label": "Run 3",
+    "game_image_icon": "/g4m3s/run3editor/loading.png",
+    "game_url": "/g4m3s/run3editor/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "70": {
+    "label": "Sausage Flip",
+    "game_image_icon": "/assets/img/sausage.jpg",
+    "game_url": "/g4m3s/sausage/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "71": {
+    "label": "Sandstrike.io",
+    "game_image_icon": "/assets/img/sanstrike.webp",
+    "game_url": "/g4m3s/sanstrike.io/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "72": {
+    "label": "Color Tunnel",
+    "game_image_icon": "/g4m3s/colourtunnel/TemplateData/logo.png",
+    "game_url": "/g4m3s/colourtunnel",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "73": {
+    "label": "Polytrack",
+    "game_image_icon": "/assets/img/poly.jpeg",
+    "game_url": "/g4m3s/polytrack",
+    "categories": [
+      "Car"
+    ]
+  },
+  "74": {
+    "label": "Friday Night Funkin",
+    "game_image_icon": "/g4m3s/fnfweek7/friday-night-funkin.png",
+    "game_url": "/g4m3s/fnfweek7/",
+    "categories": [
+      "Misc"
+    ]
+  },
+  "75": {
+    "label": "Last Horizon",
+    "game_image_icon": "/assets/img/lasthorizon1.jfif",
+    "game_url": "/g4m3s/lasthorizon1/",
+    "categories": [
+      "Adventure"
+    ]
+  },
+  "76": {
+    "label": "Fruit Ninja",
+    "game_image_icon": "/g4m3s/fruitninja/FruitNinjaTeaser.jpg",
+    "game_url": "/g4m3s/fruitninja/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "77": {
+    "label": "Rocket Pult",
+    "game_image_icon": "/assets/img/rocketpult.png",
+    "game_url": "/g4m3s/rocketpult/",
+    "categories": [
+      "Action"
+    ]
+  },
+  "78": {
+    "label": "Block Blast",
+    "game_image_icon": "/assets/img/blockblast.jpeg",
+    "game_url": "/g4m3s/blockblast/",
+    "categories": [
+      "Misc"
+    ]
+  },
+  "79": {
+    "label": "Moto X3m",
+    "game_image_icon": "/assets/img/moto.jfif",
+    "game_url": "/g4m3s/mx3m",
+    "categories": [
+      "Car"
+    ]
+  },
+  "80": {
+    "label": "Moto X3m Winter",
+    "game_image_icon": "/assets/img/moto-x3m-winter.jpeg",
+    "game_url": "/g4m3s/mx3m-winter",
+    "categories": [
+      "Car"
+    ]
+  },
+  "81": {
+    "label": "Moto X3m Spookyland",
+    "game_image_icon": "/assets/img/spookyland.png",
+    "game_url": "/g4m3s/mx3m-spookyland",
+    "categories": [
+      "Car"
+    ]
+  },
+  "82": {
+    "label": "Moto X3m Pool Party",
+    "game_image_icon": "/assets/img/poolparty.png",
+    "game_url": "/g4m3s/mx3m-poolparty",
+    "categories": [
+      "Car"
+    ]
+  },
+  "83": {
+    "label": "Eggy car",
+    "game_image_icon": "/assets/img/eggycar.png",
+    "game_url": "/g4m3s/eggycar",
+    "categories": [
+      "Car"
+    ]
+  },
+  "84": {
+    "label": "Bob the robber",
+    "game_image_icon": "/assets/img/bob.jpg",
+    "game_url": "/g4m3s/BobTheRobber",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "85": {
+    "label": "Bob the robber 2",
+    "game_image_icon": "/assets/img/bob2.jpeg",
+    "game_url": "/g4m3s/BobTheRobber2",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "86": {
+    "label": "Bob the robber 3",
+    "game_image_icon": "/assets/img/bob3.jfif",
+    "game_url": "/g4m3s/BobTheRobber3",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "87": {
+    "label": "Bob the robber 4",
+    "game_image_icon": "/assets/img/bob4.jpg",
+    "game_url": "/g4m3s/BobTheRobber4",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "88": {
+    "label": "2048",
+    "game_image_icon": "/assets/img/2048.png",
+    "game_url": "/g4m3s/2048",
+    "categories": [
+      "Misc"
+    ]
+  },
+  "89": {
+    "label": "Tetris",
+    "game_image_icon": "/assets/img/tetris.png",
+    "game_url": "/g4m3s/tetris",
+    "categories": [
+      "Misc"
+    ]
+  },
+  "90": {
+    "label": "Crazy Cattle",
+    "game_image_icon": "/assets/img/crazycattle.jpg",
+    "game_url": "/g4m3s/crazycattle",
+    "categories": [
+      "Action"
+    ]
+  },
+  "91": {
+    "label": "Snake is Mlg Edition",
+    "game_image_icon": "/assets/img/snakemlg.jfif",
+    "game_url": "/g4m3s/snakemlg",
+    "categories": [
+      "Action"
+    ]
+  },
+  "92": {
+    "label": "Tower of Destiny",
+    "game_image_icon": "/assets/img/towerofdestiny.png",
+    "game_url": "/g4m3s/TowerOfDestiny",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "93": {
+    "label": "Henry Stickman",
+    "game_image_icon": "/assets/img/escape.png",
+    "game_url": "/g4m3s/Henry",
+    "categories": [
+      "Adventure"
+    ]
+  },
+  "94": {
+    "label": "Henry Stickman  - Breaking the Bank",
+    "game_image_icon": "/assets/img/breakingbad.png",
+    "game_url": "/g4m3s/breaking-the-bank",
+    "categories": [
+      "Adventure"
+    ]
+  },
+  "95": {
+    "label": "Henry Stickman  - Fleeing the Complex",
+    "game_image_icon": "/assets/img/fleeing.png",
+    "game_url": "/g4m3s/fleeing-the-complex",
+    "categories": [
+      "Adventure"
+    ]
+  },
+  "96": {
+    "label": "Henry Stickman  - Inflitrating the Airship",
+    "game_image_icon": "/assets/img/airshiplikeamongus.png",
+    "game_url": "/g4m3s/infiltrating-the-airship",
+    "categories": [
+      "Adventure"
+    ]
+  },
+  "97": {
+    "label": "Henry Stickman  - Stealing the Diamond",
+    "game_image_icon": "/assets/img/diamond.png",
+    "game_url": "/g4m3s/stealing-the-diamond",
+    "categories": [
+      "Adventure"
+    ]
+  },
+  "98": {
+    "label": "Bounce Masters",
+    "game_image_icon": "/assets/img/bounce.jpg",
+    "game_url": "/g4m3s/bouncemasters/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "99": {
+    "label": "Bow Masters",
+    "game_image_icon": "/assets/img/bowmaster.png",
+    "game_url": "/g4m3s/bow-masters/",
+    "categories": [
+      "Platformer"
+    ]
+  },
+  "100": {
+    "label": "Amaze",
+    "game_image_icon": "/assets/img/amaze.png",
+    "game_url": "/g4m3s/amaze/",
+    "categories": [
+      "Misc"
+    ]
+  },
+  "101": {
+    "label": "Bridge Race",
+    "game_image_icon": "/assets/img/bridgerace.jpg",
+    "game_url": "/g4m3s/bridge-race/",
+    "categories": [
+      "Idle"
+    ]
+  },
+  "102": {
+    "label": "State.io",
+    "game_image_icon": "/assets/img/state-io.png",
+    "game_url": "/g4m3s/state-io/",
+    "categories": [
+      "Idle"
+    ]
+  }
+};
---
+++ assets/data/popular.js
@@ -0,0 +1,15 @@
+/* Edit this file to control which games appear in the Popular section.
+ * Use exact labels from assets/data/games.json
+ */
+window.popularGames = [
+  "Bitlife",
+  "Slope",
+  "Cookie Clicker",
+  "Yohoho.io",
+  "Funny Shooter",
+  "Retro Football",
+  "Madalin Stunt Cars",
+  "Drive Mad",
+  "Subway Surfers",
+  "Basketball Stars",
+];
